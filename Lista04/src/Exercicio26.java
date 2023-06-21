ad123b1b79a708fb589d238efdc60fd7af946189
ArthurSyngL <133982109+ArthurSyngL@users.noreply.github.com>
2023-06-20 21:09:23 -0300

    Listas

--- Lista04/src/Exercicio26.java
+++ Lista04/src/Exercicio26.java
@@ -4,7 +4,29 @@ public class Exercicio26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int QTD_ELEMENTOS = 4; // Defina o número de elementos desejado
+        int[] vetor = new int[QTD_ELEMENTOS];
+        int somatorio = 0;
+        float media, variacoes = 0, sigma, v;
 
-        
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < QTD_ELEMENTOS; i++) {
+            System.out.print("Digite um número: ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < QTD_ELEMENTOS; i++) {
+            somatorio += vetor[i];
+        }
+
+        media = (float) somatorio / QTD_ELEMENTOS;
+
+        for (int i = 0; i < QTD_ELEMENTOS; i++) {
+            v = vetor[i] - media;
+            variacoes += v * v;
+        }
+
+        sigma = (float) Math.sqrt(variacoes / QTD_ELEMENTOS);
+        System.out.printf("Resultado d = %.2f\n", sigma);
     }
 }
