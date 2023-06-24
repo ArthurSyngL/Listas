896b07bdca2472b344fe12ad6f97e349932501a4
ArthurSyngL <133982109+ArthurSyngL@users.noreply.github.com>
2023-06-24 20:47:43 -0300

    listas

--- Lista04/src/Exercicio34.java
+++ Lista04/src/Exercicio34.java
@@ -1,5 +1,35 @@
 public class Exercicio34 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int contador = 0;
 
+        System.out.println("Digite 10 números diferentes:");
+
+        while (contador < 10) {
+            int numero = sc.nextInt();
+
+            if (numeroJaDigitado(numeros, numero)) {
+                System.out.println("Número já digitado. Digite outro número:");
+            } else {
+                numeros[contador] = numero;
+                contador++;
+            }
+        }
+
+        System.out.println("Vetor final:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+
+    public static boolean numeroJaDigitado(int[] vetor, int numero) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numero) {
+                return true;
+            }
+        }
+        return false;
     }
 }
